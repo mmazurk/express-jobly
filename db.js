5be3586ae5cc4729f1846eb63580cfe382155a4e
markmazurk <markmazurk@gmail.com>
2023-06-25 10:48:58 -0700

    first commit

---
+++ db.js
@@ -0,0 +1,39 @@
+"use strict";
+/** Database setup for jobly. */
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
+
+let db;
+
+// if (process.env.NODE_ENV === "production") {
+//   db = new Client({
+//     connectionString: getDatabaseUri(),
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+//   });
+// } else {
+//   db = new Client({
+//     connectionString: getDatabaseUri()
+//   });
+// }
+
+// added for Ubuntu in WSL2
+if (process.env.NODE_ENV === "production") {
+  db = new Client({
+    host: "/var/run/postgresql/",
+    database: getDatabaseUri(),
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  db = new Client({
+    host: "/var/run/postgresql/",
+    database: getDatabaseUri()
+  });
+}
+
+db.connect();
+
+module.exports = db;
